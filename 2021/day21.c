895015effe612c45e461dc51cc51705894cee0b2
Tatone26 <anatolesainero@gmail.com>
2023-11-21 13:24:24 +0100

    day21, instant speed

--- 2021/day21.c
+++ 2021/day21.c
@@ -5,12 +5,21 @@
 #include <string.h>
 #include <limits.h>
 
+
+#define MAX_POS 10 // The max position.
+#define MIN_POS 1  // The min position.
+
+#define WIN_SCORE 28 // Win score for part two. 
+#define MAX_SCORE (WIN_SCORE - 1 + MAX_POS) // The maximum score anyone can get : 20 + 10. (part2)
+#define MIN_SCORE 0
+
 // An... EASY day ??? for the 21 ? I'm really suprised (I'm doing this in november 2023)
 // Easy to get from 27 possibles outputs to only 7 (always the sum of three dice), but that was not really a trap.
-// I guess you just needed to understant the rules. And they are not hard. Or maybe there was a really smart mathematical answer. Check the reddit. 
-// Fun to optimize ! (I like that)
-// I like the fact that the example output takes almost twice as long as my input before optimisations.
-// I guess there is one more recursion cycle. 
+// I guess you just needed to understand the rules to make a solution that runs in a few seconds max. 
+// To get really low, just need to save every already-computed solutions #caching
+
+// Proud of my made-up "hash table", which I have been able to make thanks to the max values of every int
+// (score <= 30 and pos <= 10), and I even made it dynamic if you want to change the parameters. (max win_score is 28 before long int)
 
 void readInput(FILE *f, fpos_t *start, int *playerOne, int *playerTwo)
 {
@@ -30,14 +39,12 @@ void readInput(FILE *f, fpos_t *start, int *playerOne, int *playerTwo)
 
 int deterministicDice(int lastRoll)
 {
-    if (lastRoll >= 100)
-        return 1;
-    return lastRoll + 1;
+    return (lastRoll + 1 - MIN_POS) % 100 + MIN_POS;
 }
 
 int movePlayer(int lastPos, int roll)
 {
-    return (lastPos + roll - 1) % 10 + 1;
+    return (lastPos + roll - MIN_POS) % MAX_POS + MIN_POS;
 }
 
 int action(int startOne, int startTwo)
@@ -61,6 +68,7 @@ int action(int startOne, int startTwo)
         {
             return scoreTwo * numberOfRolls;
         }
+
         roll1 = deterministicDice(lastRoll);
         roll2 = deterministicDice(roll1);
         roll3 = deterministicDice(roll2);
@@ -75,35 +83,51 @@ int action(int startOne, int startTwo)
     }
 }
 
-// Now freaking optimise this ! I like that. 
-long int result(int pos1, int pos2, int score1, int score2, int playerToMove)
+// Useful : 0 <= pos - 1 < 10 so can use (pos1 - 1) + 10 * (pos2 - 1) without any problems -> between 0 and 99
+// 0 <= score <= 30 (extreme case) so (score1) + 30 * (score2) no problem
+// that gets us to ((pos1 - 1) + 10 * (pos2 - 1)) + 10 * 10 * ((score1) + 30 * (score2)) -> no ambiguities -> between 0 and 93099.
+// move all of that to the left once and set the last number as playerToMove, and we get something between 0 and 186198.
+// And it works !
+int hash(int pos1, int pos2, int score1, int score2, int playerToMove)
 {
-    if (score1 >= 21)
+    return ((((pos1 - MIN_POS) + MAX_POS * (pos2 - MIN_POS)) + MAX_POS * MAX_POS * ((score1 - MIN_SCORE) + MAX_SCORE * (score2 - MIN_SCORE))) << 1) | playerToMove;
+}
+
+// Need to cache some stuff : some situations can be reached multiple ways.
+long int result(int pos1, int pos2, int score1, int score2, int playerToMove, long int *cache)
+{
+    if (score1 >= WIN_SCORE)
         return 1;
-    if (score2 >= 21)
+    if (score2 >= WIN_SCORE)
         return 0;
+
+    int h = hash(pos1, pos2, score1, score2, playerToMove); // The magiiiic
+    if (cache[h] != -1)
+        return cache[h];
+
+    long int results[7];
     if (playerToMove)
     {
-        long int one = result(pos1, movePlayer(pos2, 3), score1, score2 + movePlayer(pos2, 3), 0);
-        long int two = result(pos1, movePlayer(pos2, 4), score1, score2 + movePlayer(pos2, 4), 0);
-        long int three = result(pos1, movePlayer(pos2, 5), score1, score2 + movePlayer(pos2, 5), 0);
-        long int four = result(pos1, movePlayer(pos2, 6), score1, score2 + movePlayer(pos2, 6), 0);
-        long int five = result(pos1, movePlayer(pos2, 7), score1, score2 + movePlayer(pos2, 7), 0);
-        long int six = result(pos1, movePlayer(pos2, 8), score1, score2 + movePlayer(pos2, 8), 0);
-        long int seven = result(pos1, movePlayer(pos2, 9), score1, score2 + movePlayer(pos2, 9), 0);
-        return one + 3*two + 6*three + 7*four + 6*five + 3 *six + seven;
-    } else 
-    {
-        long int one = result(movePlayer(pos1, 3), pos2, score1 + movePlayer(pos1, 3), score2, 1);
-        long int two = result(movePlayer(pos1, 4), pos2, score1 + movePlayer(pos1, 4), score2, 1);
-        long int three = result(movePlayer(pos1, 5), pos2, score1 + movePlayer(pos1, 5), score2, 1);
-        long int four = result(movePlayer(pos1, 6), pos2, score1 + movePlayer(pos1, 6), score2, 1);
-        long int five = result(movePlayer(pos1, 7), pos2, score1 + movePlayer(pos1, 7), score2, 1);
-        long int six = result(movePlayer(pos1, 8), pos2, score1 + movePlayer(pos1, 8), score2, 1);
-        long int seven = result(movePlayer(pos1, 9), pos2, score1 + movePlayer(pos1, 9), score2, 1);
-        return one + 3*two + 6*three + 7*four + 6*five + 3 *six + seven;
+        int newPos = pos2 + 2;
+        for (int i = 3 - 3; i <= 9 - 3; i++)
+        {
+            newPos = newPos % 10 + 1;
+            results[i] = result(pos1, newPos, score1, score2 + newPos, 0, cache);
+        }
     }
-
+    else
+    {
+        int newPos = pos1 + 2; // It will move at least two steps, then a 3rd, 4th, 5th, 6th, 7th, 8th or 9th, but nothing else. 
+        for (int i = 3 - 3; i <= 9 - 3; i++)
+        {
+            newPos = newPos % 10 + 1;
+            results[i] = result(newPos, pos2, score1 + newPos, score2, 1, cache);
+        }
+    }
+    // Simple probas, make a proba tree -> 27 possibles outputs, and their sums are what I used here. 
+    long int THEresult = results[0] + 3 * results[1] + 6 * results[2] + 7 * results[3] + 6 * results[4] + 3 * results[5] + results[6];
+    cache[h] = THEresult;
+    return THEresult;
 }
 
 int main()
@@ -118,9 +142,17 @@ int main()
     fgetpos(f, &start);
     int startOne, startTwo;
     readInput(f, &start, &startOne, &startTwo);
-    printf("-- Day 21 --\nTo 1000 : loser score * numbers of rolls : %d\n", action(startOne, startTwo));
+    printf("-- Day 21 --\nTo 1000, deterministic : loser score * numbers of rolls : %d\n", action(startOne, startTwo));
 
-    printf("QUANTUM DIE : First player win in %ld universes.\n", result(startOne, startTwo, 0, 0, 0));
+    int maxHash = hash(MAX_POS, MAX_POS, MAX_SCORE, MAX_SCORE, 1);
+    long int cache[maxHash];
+    for (int i = 0; i < maxHash; i++)
+    {
+        cache[i] = -1;
+    }
+    long int part2 = result(startOne, startTwo, MIN_SCORE, MIN_SCORE, 0, cache);
+
+    printf("QUANTUM DIE : First player win in %ld universes.\n", part2);
     fclose(f);
     return 0;
 }
