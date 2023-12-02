f9170f4782256d00861936723cbc9a604413aa8d
Tatone26 <anatolesainero@gmail.com>
2023-12-02 13:42:55 +0100

    fmt

--- 2023/src/days/day03.rs
+++ 2023/src/days/day03.rs
@@ -1,9 +1,8 @@
 use crate::{Solution, SolutionPair};
 
-
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve(_filename : &'static str) -> SolutionPair {
+pub fn solve(_filename: &'static str) -> SolutionPair {
     // Your solution here...
     let sol1: u64 = 0;
     let sol2: u64 = 0;
