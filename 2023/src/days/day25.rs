460694f279173b4c8d1cd28e48e09860af59fb0b
Tatone26 <anatolesainero@gmail.com>
2023-12-25 13:09:37 +0100

    day25 manually

--- 2023/src/days/day25.rs
+++ 2023/src/days/day25.rs
@@ -1,9 +1,37 @@
+use std::io::Write;
+
+use itertools::Itertools;
+use petgraph::{
+    dot::{Config, Dot},
+    graphmap::UnGraphMap,
+    visit::{EdgeIndexable, IntoEdges},
+};
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve(_buffer: &str) -> SolutionPair {
-    // Your solution here...
+// UnGraphMap ?
+
+fn read_input(buffer: &str) {
+    let nodes = buffer.lines().flat_map(|line| {
+        let mut it = line.split(": ");
+        let name = it.next().unwrap();
+        let linked = it.next().unwrap().split_ascii_whitespace().collect_vec();
+        linked.iter().map(|&p| (name, p)).collect_vec()
+    });
+
+    let mut graph = UnGraphMap::<_, ()>::from_edges(nodes);
+    graph.remove_edge("tqn", "tvf");
+    graph.remove_edge("vzb", "tnr");
+    graph.remove_edge("lmg", "krx");
+    for i in petgraph::algo::tarjan_scc(&graph) {
+        println!("{}", i.len());
+    }
+}
+
+pub fn solve(buffer: &str) -> SolutionPair {
+    read_input(buffer);
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
@@ -12,9 +40,16 @@ pub fn solve(_buffer: &str) -> SolutionPair {
 
 #[test]
 fn test() {
-    let (Solution::U64(s1), Solution::U64(s2)) = solve("input/test_day25.txt") else {
+    let (Solution::U64(s1), Solution::U64(s2)) =
+        solve(&std::fs::read_to_string("input/test_day25.txt").unwrap())
+    else {
         panic!("Mauvais type de solutions pour le test.")
     };
-    assert_eq!(s1, 0);
+    assert_eq!(s1, 54);
     assert_eq!(s2, 0);
 }
+
+// for my input : tqn-tvf is a separator.
+// "lmg" - "krx"
+// "vzb" - "tnr"
+// result : 746 and 781.
